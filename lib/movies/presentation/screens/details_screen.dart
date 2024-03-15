ccd53568fdac8a6628adbe1273bd5be781ed8836
yasmen <yasmenmema16@gmail>
2024-03-15 17:46:20 +0200

    genre list

--- lib/movies/presentation/screens/details_screen.dart
+++ lib/movies/presentation/screens/details_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
@@ -14,7 +13,8 @@ import '../controller/movie_details_bloc.dart';
 
 class DetailsScreen extends StatefulWidget {
   const DetailsScreen({Key? key, required this.movie}) : super(key: key);
-final DiscoverMovies movie;
+  final DiscoverMovies movie;
+
   @override
   State<DetailsScreen> createState() => _DetailsScreenState();
 }
@@ -30,167 +30,233 @@ class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => MovieDetailsBloc(getIt<MovieDetailsUseCase>())..add(GetMovieDetailsEvent(widget.movie!.id! , Constants.API_KEY)),
+      create: (context) => MovieDetailsBloc(getIt<MovieDetailsUseCase>())
+        ..add(GetMovieDetailsEvent(widget.movie!.id!, Constants.API_KEY)),
       child: Scaffold(
         body: CustomScrollView(
           slivers: [
-             SliverAppBar.medium(
-            backgroundColor: Colours.PrimaryColor,
-            expandedHeight: 300,
-            pinned: true,
-            floating: true,
-            leading:  IconButton(
-              icon: Icon(Icons.arrow_back_ios_new , color: Colors.white,),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              // title: Text('${widget.movie?.title}',
-              //   style: TextStyle(fontFamily: 'AABB', fontSize: 16, color: Colors.white),
-              // ),
-              background: Image.network(
-                '${Constants.IMAGES_BASE_URL}${widget.movie?.backdropPath}',
-                filterQuality: FilterQuality.high,
-                fit: BoxFit.cover,
+            SliverAppBar.medium(
+              backgroundColor: Colours.PrimaryColor,
+              expandedHeight: 300,
+              pinned: true,
+              floating: true,
+              leading: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
               ),
-
+              flexibleSpace: FlexibleSpaceBar(
+                // title: Text('${widget.movie?.title}',
+                //   style: TextStyle(fontFamily: 'AABB', fontSize: 16, color: Colors.white),
+                // ),
+                background: Image.network(
+                  '${Constants.IMAGES_BASE_URL}${widget.movie?.backdropPath}',
+                  filterQuality: FilterQuality.high,
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
+            SliverToBoxAdapter(
+                child: BlocBuilder<MovieDetailsBloc, MovieDetailsState>(
+              buildWhen: (previousState, currentState) =>
+                  previousState.movieDetailsState !=
+                  currentState.movieDetailsState,
+              builder: (BuildContext context, MovieDetailsState state) {
+                switch (state.movieDetailsState) {
+                  case RequestState.isLoading:
+                    return const SizedBox(
+                        height: 200,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: Colours.SecondryColor,
+                          ),
+                        ));
+                  case RequestState.isLoaded:
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child:
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('${state.movieDetails?.title}',
+                              style: TextStyle(
+                                  fontFamily: 'AABB',
+                                  fontSize: 22,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.confirmation_num_sharp,
+                                color: Colours.GrayColor,
+                                size: 15,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text('${state.movieDetails?.voteCount} Count',
+                                  style: TextStyle(
+                                      fontFamily: 'AABB',
+                                      fontSize: 12,
+                                      color: Colours.GrayColor)),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Icon(
+                                Icons.star,
+                                color: Colours.GrayColor,
+                                size: 15,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                  '${state.movieDetails?.voteAverage?.toStringAsFixed(2)} (IMDP)',
+                                  style: TextStyle(
+                                      fontFamily: 'AABB',
+                                      fontSize: 12,
+                                      color: Colours.GrayColor)),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Divider(
+                            color: Colors.grey.withOpacity(.2),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Release date',
+                                        style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 14,
+                                            color: Colors.white)),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text('${state.movieDetails?.releaseDate}',
+                                        style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 12,
+                                            color: Colours.GrayColor)),
+                                  ],
+                                ),
+                              ),
+                              // SizedBox(
+                              //   width: 20,
+                              // ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Popularity',
+                                        style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 14,
+                                            color: Colors.white)),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text('${state.movieDetails?.popularity}',
+                                        style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 12,
+                                            color: Colours.GrayColor)),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Genre',
+                                        style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 14,
+                                            color: Colors.white)),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Wrap(
+                                      children: state.movieDetails!.genres!.map((e) =>
+                                          Container(
+                                            margin: const EdgeInsets.all(2.0),
+                                            decoration: BoxDecoration(
+                                                borderRadius:
+                                                BorderRadius.circular(10),
+                                                color:
+                                                Colors.grey.withOpacity(.2)),
+                                            child: Padding(
+                                        padding: const EdgeInsets.all(4.0),
+                                        child: new Text(e.name.toString()+' ',style: TextStyle(
+                                            fontFamily: 'AABB',
+                                            fontSize: 10,
+                                            color: Colors.white)),
 
-          ),
-          SliverToBoxAdapter(
-           child:
-           BlocBuilder<MovieDetailsBloc , MovieDetailsState>(
-             buildWhen: (previousState, currentState) =>
-             previousState.movieDetailsState != currentState.movieDetailsState,
-             builder: (BuildContext context, MovieDetailsState state) {
-               switch (state.movieDetailsState) {
-                 case RequestState.isLoading:
-                   return const SizedBox(height: 200,
-                       child: Center(
-                         child: CircularProgressIndicator(color: Colours
-                             .SecondryColor,),));
-                 case RequestState.isLoaded:
-                   return
-                     Padding(
-                       padding: const EdgeInsets.all(16.0),
-                       child: Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           SizedBox(height: 8,),
-                           Text('${state.movieDetails?.title}',
-                               style: TextStyle(fontFamily: 'AABB',
-                                   fontSize: 22,
-                                   color: Colors.white,
-                                   fontWeight: FontWeight.bold)),
-                           SizedBox(height: 8,),
-                           Row(
-                             children: [
-                               Icon(Icons.confirmation_num_sharp,
-                                 color: Colours.GrayColor, size: 15,),
-                               SizedBox(width: 4,),
-                               Text('${state.movieDetails?.voteCount} Count',
-                                   style: TextStyle(fontFamily: 'AABB',
-                                       fontSize: 12,
-                                       color: Colours.GrayColor)),
-                               SizedBox(width: 20,),
-                               Icon(Icons.star, color: Colours.GrayColor,
-                                 size: 15,),
-                               SizedBox(width: 4,),
-                               Text('${state.movieDetails?.voteAverage
-                                   ?.toStringAsFixed(2)} (IMDP)',
-                                   style: TextStyle(fontFamily: 'AABB',
-                                       fontSize: 12,
-                                       color: Colours.GrayColor)),
-
-                             ],
-                           ),
-                           SizedBox(height: 8,),
-                           Divider(color: Colors.grey.withOpacity(.2),),
-                           SizedBox(height: 8,),
-                           Row(
-                             children: [
-                               Column(
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                   Text('Release date', style: TextStyle(
-                                       fontFamily: 'AABB',
-                                       fontSize: 14,
-                                       color: Colors.white)),
-                                   SizedBox(height: 8,),
-                                   Text('${state.movieDetails?.releaseDate}',
-                                       style: TextStyle(fontFamily: 'AABB',
-                                           fontSize: 12,
-                                           color: Colours.GrayColor)),
-                                 ],
-                               ),
-                               SizedBox(width: 20,),
-                               Column(
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                   Text('Popularity', style: TextStyle(
-                                       fontFamily: 'AABB',
-                                       fontSize: 14,
-                                       color: Colors.white)),
-                                   SizedBox(height: 8,),
-                                   Text('${state.movieDetails?.popularity}',
-                                       style: TextStyle(fontFamily: 'AABB',
-                                           fontSize: 12,
-                                           color: Colours.GrayColor)),
-                                 ],
-                               ),
-                               SizedBox(width: 20,),
-                               Column(
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                   Text('Genre', style: TextStyle(
-                                       fontFamily: 'AABB',
-                                       fontSize: 14,
-                                       color: Colors.white)),
-                                   SizedBox(height: 8,),
-                                   Text('${state.movieDetails?.genres?.first.name}',
-                                       style: TextStyle(fontFamily: 'AABB',
-                                           fontSize: 12,
-                                           color: Colours.GrayColor)),
-                                 ],
-                               ),
-
-                             ],
-                           ),
-                           SizedBox(height: 8,),
-                           Divider(color: Colors.grey.withOpacity(.2),),
-                           SizedBox(height: 8,),
-                           Text('Description', style: TextStyle(
-                               fontFamily: 'AABB',
-                               fontSize: 15,
-                               color: Colors.white)),
-                           SizedBox(height: 8,),
-                           Text('${state.movieDetails?.overview}',
-                               style: TextStyle(fontFamily: 'AABB',
-                                 fontSize: 14,
-                                 color: Colours.GrayColor,)),
-
-                         ],
-                       ),
-                     );
-                 case RequestState.isError:
-                   return SizedBox(
-                     height: 400,
-                     child: Text(
-                       state.movieDetailsMessage,
-                     ),
-                   );
-               }
-             },
-
-           )
-
-
-         )
-
-         ],
-       ),
-
+                                      ),
+                                          )).toList(),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Divider(
+                            color: Colors.grey.withOpacity(.2),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Description',
+                              style: TextStyle(
+                                  fontFamily: 'AABB',
+                                  fontSize: 15,
+                                  color: Colors.white)),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('${state.movieDetails?.overview}',
+                              style: TextStyle(
+                                fontFamily: 'AABB',
+                                fontSize: 14,
+                                color: Colours.GrayColor,
+                              )),
+                        ],
+                      ),
+                    );
+                  case RequestState.isError:
+                    return SizedBox(
+                      height: 400,
+                      child: Text(
+                        state.movieDetailsMessage,
+                      ),
+                    );
+                }
+              },
+            ))
+          ],
+        ),
       ),
     );
   }
